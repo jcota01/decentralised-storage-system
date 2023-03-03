61518b22aea30ff182dcdb0f41bc9b46eb82038c
Jack Cota <jack@cota.co.uk>
2023-03-03 23:16:33 +0000

    Update Controller to use HashMap over Vec
    I have changed the Controller to use a HashMap for storing all the nodes. This is so each node can be stored by a key: their ID, which makes finding the node in the map much easier, especially if I destroy nodes during runtime. Currently, I need to improve the get_node function so that it can handle if the ID given is not in the hashmap.

--- src/controller.rs
+++ src/controller.rs
@@ -1,18 +1,28 @@
+use std::collections::HashMap;
 use crate::node::Node;
 
 #[derive(Debug)]
 pub struct Controller {
-    pub nodes: Vec<Node>
+    pub nodes: HashMap<usize, Node>
 }
 
 impl Controller {
+    // Returns a Node from the HashMap
+    // TODO: Handle None value
+    pub fn get_node(&self, id: usize) -> &Node {
+        self.nodes.get(&id).unwrap()
+    }
+
 
     // Create a new node and add it to the vector
     pub fn add_new_node(&mut self){
-        self.nodes.push(Node::new(
+        self.nodes.insert(
             self.nodes.len() + 1,
-            "127.0.0.1".to_string(),
-            20001 + self.nodes.len(),
-        ));
+            Node::new(
+                self.nodes.len() + 1,
+                "127.0.0.1".to_string(),
+                20001 + self.nodes.len(),
+            )
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 #![allow(dead_code)]
 
 use crate::controller::Controller;
-use std::{thread, time::Duration};
+use std::collections::HashMap;
 
 mod controller;
 mod node;
 
 fn main() {
-    let mut ctrl = Controller{ nodes: Vec::new() };
+    let mut ctrl = Controller{ nodes: HashMap::new() };
 
     for _i in 1..=3{
         ctrl.add_new_node();
 
     }
 
-    thread::sleep(Duration::from_secs(1));
 
-    ctrl.nodes[0].send_msg(String::from("hello"), String::from("127.0.0.1:20002"));
-    ctrl.nodes[0].send_msg(String::from("hello"), String::from("127.0.0.1:20003"));
+    ctrl.get_node(1).send_msg(String::from("hello"), String::from("127.0.0.1:20002"));
+    ctrl.get_node(1).send_msg(String::from("hello"), String::from("127.0.0.1:20003"));
 
-    ctrl.nodes[2].send_msg(String::from("hi"), String::from("127.0.0.1:20001"));
+    ctrl.get_node(3).send_msg(String::from("hi"), String::from("127.0.0.1:20001"));
 
     loop {
 
